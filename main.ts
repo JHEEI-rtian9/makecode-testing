7e77b716ca1f76ae77410cd1419068406e67a000
JHEEI-rtian9 <rtian9@jh.edu>
2024-07-09 00:52:49 +0800

    test ButtonEvent.Click limits

--- main.ts
+++ main.ts
@@ -1,7 +1,28 @@
+/**
+ * Adafruit didn't update Makecode version, so you CANNOT use a github repo
+ */
+/**
+ * Line above must be empty
+ * 
+ * Empty line between every line or else it concats to the previous line
+ * 
+ * Empty line below
+ */
+
 input.buttonA.onEvent(ButtonEvent.Click, function () {
     console.log("Temperature: " + input.temperature(TemperatureUnit.Celsius) + " at time: " + control.timer1.millis() + " milliseconds")
 })
 input.buttonB.onEvent(ButtonEvent.Click, function () {
     console.log("Sound Level: " + input.soundLevel() + " at time: " + control.timer1.millis() + " milliseconds")
 })
+input.buttonB.onEvent(ButtonEvent.Down, function() {
+    presstime = control.timer1.millis()
+    console.log("B Button Down")
+})
+input.buttonB.onEvent(ButtonEvent.Up, function(){
+    presstime -= Math.abs(control.timer1.millis())
+    console.log("B Button UP")
+    console.log("Presstime: "+presstime)
+})
 control.timer1.reset()
+let presstime = 0
