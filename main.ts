2f1de60c60cb29688a6064bb8abb26e088fbb5d5
JHEEI-rtian9 <rtian9@jh.edu>
2024-07-08 23:19:59 +0800

    made logging tied to button presses + timer records

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-console.log("Sound Level: " + input.soundLevel() + "; Temperature: " + input.temperature(TemperatureUnit.Celsius) + " Celsius")
+input.buttonA.onEvent(ButtonEvent.Click, function () {
+    console.log("Temperature: " + input.temperature(TemperatureUnit.Celsius) + " at time: " + control.timer1.millis() + " milliseconds")
+})
+input.buttonB.onEvent(ButtonEvent.Click, function () {
+    console.log("Sound Level: " + input.soundLevel() + " at time: " + control.timer1.millis() + " milliseconds")
+})
+control.timer1.reset()
