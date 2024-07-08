1c8090ae199af8b7d8bb529d9a8cc53a7e5ae1d0
JHEEI-rtian9 <rtian9@jh.edu>
2024-07-09 01:00:29 +0800

    Click has limit of 1000ms (1s)

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+input.buttonB.onEvent(ButtonEvent.Down, function () {
+    presstime = control.timer1.millis()
+    console.log("B Button Down")
+})
+// Limit of ButtonEvent.Click is 1 second, otherwise is ButtonEvent.LongClick
+input.buttonA.onEvent(ButtonEvent.Click, function () {
+    console.log("Temperature: " + input.temperature(TemperatureUnit.Celsius) + " at time: " + control.timer1.millis() + " milliseconds")
+})
+input.buttonB.onEvent(ButtonEvent.Up, function () {
+    presstime += 0 - control.timer1.millis()
+    console.log("B Button UP")
+    console.log("Presstime: " + Math.abs(presstime))
+})
+input.buttonB.onEvent(ButtonEvent.Click, function () {
+    console.log("Sound Level: " + input.soundLevel() + " at time: " + control.timer1.millis() + " milliseconds")
+})
 /**
  * Adafruit didn't update Makecode version, so you CANNOT use a github repo
  */
@@ -8,21 +24,5 @@
  * 
  * Empty line below
  */
-
-input.buttonA.onEvent(ButtonEvent.Click, function () {
-    console.log("Temperature: " + input.temperature(TemperatureUnit.Celsius) + " at time: " + control.timer1.millis() + " milliseconds")
-})
-input.buttonB.onEvent(ButtonEvent.Click, function () {
-    console.log("Sound Level: " + input.soundLevel() + " at time: " + control.timer1.millis() + " milliseconds")
-})
-input.buttonB.onEvent(ButtonEvent.Down, function() {
-    presstime = control.timer1.millis()
-    console.log("B Button Down")
-})
-input.buttonB.onEvent(ButtonEvent.Up, function(){
-    presstime -= Math.abs(control.timer1.millis())
-    console.log("B Button UP")
-    console.log("Presstime: "+presstime)
-})
-control.timer1.reset()
 let presstime = 0
+control.timer1.reset()
